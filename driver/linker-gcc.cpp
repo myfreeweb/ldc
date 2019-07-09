573cb1456b1074bb4ab604a838658f30beb6a797
Greg V <greg@unrelenting.technology>
2019-07-09 12:57:07 +0300

    Link druntime to libexecinfo on FreeBSD for backtrace

--- driver/linker-gcc.cpp
+++ driver/linker-gcc.cpp
@@ -638,6 +638,8 @@ void ArgsBuilder::addDefaultPlatformLibs() {
     args.push_back("-ldl");
   // fallthrough
   case llvm::Triple::FreeBSD:
+    args.push_back("-lexecinfo"); // for static druntime
+  // fallthrough
   case llvm::Triple::NetBSD:
   case llvm::Triple::OpenBSD:
   case llvm::Triple::DragonFly:
